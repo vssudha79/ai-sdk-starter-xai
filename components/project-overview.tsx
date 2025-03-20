8f32a0612471592334bcd4fcf1e711700376c566
Jared Palmer <jared@jaredpalmer.com>
2025-03-20 12:02:23 -0400

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -2,7 +2,7 @@ import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
-      <h1 className="text-3xl font-semibold mb-4">Vercel x xAI Chatbot</h1>
+      <h1 className="text-3xl font-semibold mb-4">Vercel{" "}<span className="text-zinc-500">+</span>{" "}xAI Chatbot</h1>
       <p className="text-center">
         This starter project uses <Link href="https://xai.com/">xAI</Link> with
         the <Link href="https://sdk.vercel.ai/docs">AI SDK</Link> via the{" "}
